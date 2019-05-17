2024a906f0cb98183f15c2a2c66921d654b9bd82
Maksim Falei <Maksim.Falei@itechart-group.com>
2019-05-17 18:10:39 +0300

    Made responsive registration form Added password confirm validation Made onSubmit method

--- src/components/RegForm/RegForm.js
+++ src/components/RegForm/RegForm.js
@@ -1,50 +1,87 @@
-import { Component } from "react";
-import "./reg-form.scss";
+import { Component } from 'react';
+import './reg-form.scss';
 
 class RegForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      userData: {}
+      isPostedData: false,
+      isPswdConfirmed: true
     };
   }
-/**
- * @prop e {string}
- */
-  onSubmit = (e) => {
+  /**
+   * @param {Event} e
+   */
+  onSubmit = e => {
     e.preventDefault();
-  }
+    let formData = new FormData(e.target);
+    let isAllInputsFilled = false;
+
+    for (let input of formData.entries()) {
+      input[1] ? (isAllInputsFilled = true) : (isAllInputsFilled = false);
+    }
+
+    const pswd = formData.get('pswd'),
+      confirmPswd = formData.get('confirmPswd');
+
+    if (pswd !== confirmPswd) {
+      this.setState({ isPswdConfirmed: false });
+    } else {
+      !this.state.isPswdConfirmed && this.setState({ isPswdConfirmed: true });
+    }
+
+    if (isAllInputsFilled && this.state.isPswdConfirmed) {
+      fetch('/dbImitation/films.json', {
+        method: 'POST',
+        body: formData
+      })
+        .then(res => this.setState({ isPostedData: true }))
+        .catch(err => console.log(err));
+    }
+  };
 
   render() {
+    let { isPswdConfirmed } = this.state;
     return (
       <div className="reg-form">
-      <div className="reg-form-header">Registration</div>
-        <form onSubmit={this.onSubmit}>
+        <div className="reg-form-header">Registration</div>
+        <form onSubmit={this.onSubmit} autoComplete="on">
           <div className="form-group">
             <label htmlFor="user-name">Username:</label>
-            <input
-              type="text"
-              placeholder="Desired username"
-              name="user-name"
-              id="user-name"
-            />
+            <div className="input-group">
+              <input type="text" placeholder="Desired username" name="userName" id="user-name" autoFocus required />
+            </div>
           </div>
           <div className="form-group">
             <label htmlFor="email">Email:</label>
-            <input type="email" name="email" id="email" placeholder='example@example.com'/>
+            <div className="input-group">
+              <input type="email" name="email" id="email" placeholder="example@example.com" required />
+            </div>
           </div>
           <div className="form-group">
             <label htmlFor="pswd">Password:</label>
-            <input type="password" name="pswd" id="pswd" placeholder='password'/>
+            <div className="input-group">
+              <input type="password" name="pswd" id="pswd" placeholder="password" required />
+            </div>
           </div>
           <div className="form-group">
-          <label htmlFor="repeat-pswd">Repeat password:</label>
-            <input type="password" name="repeat-pswd" id="repeat-paswd" placeholder='password'/>
-          </div>
-            <div className="form-group reg-form-btn">
-              <button>Registration</button>
+            <label htmlFor="confirm-pswd">Confirm password:</label>
+            <div className="input-group">
+              <input
+                type="password"
+                name="confirmPswd"
+                id="confirm-pswd"
+                className={isPswdConfirmed ? '' : 'wrong-input'}
+                placeholder="confirm password"
+                required
+              />
             </div>
+          </div>
+          <div className="form-group reg-form-btn">
+            <button type="submit">Registration</button>
+          </div>
+          {!isPswdConfirmed && <span className="reg-form-confirm-tip">Please confirm your password</span>}
         </form>
       </div>
     );
