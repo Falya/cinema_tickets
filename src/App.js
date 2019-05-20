9ab7884654160e746c7aa00d51dd3905f586cfbf
Maksim Falei <Maksim.Falei@itechart-group.com>
2019-05-20 16:39:23 +0300

    first commit for main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
+import MainPage from './components/MainPage/MainPage';
 
 class App extends Component {
   render() {
     return (
       <div className='wrapper'>
         <Header />
+        <MainPage />
       </div>
     );
   }
---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './main-page.scss';
+
+class MainPage extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+
+
+  render() {
+    return (
+      <section>
+
+      </section>
+    );
+  }
+}
+
+
+export default MainPage;
