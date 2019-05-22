d51e458b718208a70affc14afb3d4a5beb9a9841
Maksim Falei <Maksim.Falei@itechart-group.com>
2019-05-22 14:33:59 +0300

    Made main-page

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 import './main-page.scss';
 
 class MainPage extends Component {
   constructor(props) {
     super(props);
-
   }
-
-
 
   render() {
     return (
       <section>
-      <div className="heading">
-        <div className="heading-header">
-          <h2>Now in the cinema</h2>
+        <div className="heading">
+          <div className="heading-header">
+            <h2>Now in the cinema</h2>
+          </div>
+          <div className="heading-body">
+            <div className="card">
+              <div className="card-body">
+                <figure>
+                  <div className="image-wrapper">
+                    <div className="card-overlay" />
+                    <img src="http://cdn.collider.com/wp-content/uploads/2018/03/avengers-infinity-war-poster.jpg" alt="" />
+                  </div>
+                  <figcaption>
+                    <p>16+/RU</p>
+                    <h3>Avengers: Endgame</h3>
+                    <span>Fantasy, Triller</span>
+                  </figcaption>
+                </figure>
+                <Button size="normal" ghost>
+                  Will book!
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="heading-body">
-          <div className="card"></div>
-        </div>
-      </div>
-      <div className="heading">
-
-      </div>
       </section>
     );
   }
 }
-
 
 export default MainPage;
