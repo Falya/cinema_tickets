b1b645fd251afe6571890bb5c1515a4f91fd6887
Maksim <anger65@gmail.com>
2019-06-03 18:41:48 +0300

    Made consts

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './main-page.scss';
 import Heading from './Heading/Heading';
+const SHOW_BOOKING_PAGE = 'showBookingPage';
+const SHOW_MOVIE_PAGE = 'showMoviePage';
 
 class MainPage extends Component {
   constructor(props) {
@@ -8,9 +10,9 @@ class MainPage extends Component {
   }
 
   cardMethod = (id, method) => {
-    if (method === 'showBookingPage') {
+    if (method === SHOW_BOOKING_PAGE) {
       this.props.showBookingPage(id);
-    } else if (method === 'showMoviePage') {
+    } else if (method === SHOW_MOVIE_PAGE) {
       console.log(`Open movie page for ${id}`);
     }
   };
