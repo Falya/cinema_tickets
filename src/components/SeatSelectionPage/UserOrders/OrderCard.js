b0b77f2eb4e2f0829a079c4b979cb853c6157d38
Maksim <anger65@gmail.com>
2019-07-29 17:36:56 +0300

    Made a message to reduce the number of products

--- src/components/SeatSelectionPage/UserOrders/OrderCard.js
+++ src/components/SeatSelectionPage/UserOrders/OrderCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { unBlockSeat } from '../../../webAPI';
 import { connect } from 'react-redux';
+import message from 'antd/lib/message';
 import { getSeanceApi, setOrderFeature } from '../../../redux/actions/actions';
 import './order-card.scss';
 
@@ -43,6 +44,7 @@ class ConnectedOrderCard extends Component {
       const newFeatures = this.props.features.filter(item => item.product !== this.props.feature.product);
       newFeature.amount = 5 * this.props.blockedSeats.length;
       this.props.setOrderFeature([...newFeatures, newFeature]);
+      message.info(`The max amount of ${feature.product} will be reduced to ${5 * this.props.blockedSeats.length} pc.`);
     }
   }
 
